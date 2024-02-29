c69bc4f8a75656cf37dcabbdd24bad5211623d14
jelly <806352173@qq.com>
2024-02-29 13:11:15 +0800

    add dedaultSeo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,32 @@
 import "@/styles/globals.css";
+import { DefaultSeo } from "next-seo";
 import type { AppProps } from "next/app";
-
+const titleDefault = 'Holoworld AI';
+const urlDefault = 'https://holoworld.app';
+const descriptionDefault =
+  'Chat, play, and create with your favorite characters!';
+const imageDefault = '/icons/share.png';
+const author = 'Hologram Labs';
+export const defaultSEOConfig = {
+  author: author,
+  themeColor: '#000',
+  title: titleDefault,
+  description: descriptionDefault,
+  canonical: urlDefault,
+  openGraph: {
+    type: 'website',
+    url: urlDefault,
+    title: titleDefault,
+    description: descriptionDefault,
+    images: [{ url: imageDefault, alt: titleDefault }],
+  },
+  twitter: {
+    cardType: 'summary',
+  },
+};
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return <>
+      <DefaultSeo {...defaultSEOConfig} />
+  <Component {...pageProps} />
+  </>;
 }
