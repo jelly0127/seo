5e7434b03aa042efc4182df745fffbf9164f456e
jelly <806352173@qq.com>
2024-02-29 11:44:03 +0800

    test seo

--- src/pages/a/index.tsx
+++ src/pages/a/index.tsx
@@ -1,6 +1,4 @@
 import { NextSeo } from 'next-seo';
-import React, { useEffect, useState } from 'react';
-
 interface SeoProps {
   title: string;
   description: string;
@@ -17,47 +15,10 @@ interface SeoProps {
   };
 }
 
-const Page: React.FC = () => {
-  const [seoProps, setSeoProps] = useState<SeoProps | null>(null);
-
-  useEffect(() => {
-    // 模拟异步操作，延迟两秒钟
-    const fetchData = async () => {
-      // 假设您的实际数据获取逻辑在此
-      // 这里只是设置了一个简单的示例数据
-      const data: SeoProps = {
-        title: 'Holoworld AI- Chat',
-        description: 'Your description here',
-        canonical: 'Your canonical URL here',
-        openGraph: {
-          url: 'Your Open Graph URL here',
-          title: 'Your Open Graph Title',
-          description: 'Your Open Graph Description',
-          images: [
-            {
-              url: '/images/test.png',
-              alt: 'Your image alt text here',
-            },
-          ],
-          siteName: 'Your Site Name',
-        },
-      };
-
-      // 模拟延迟两秒钟
-      await new Promise((resolve) => setTimeout(resolve, 2000));
-
-      // 将获取的数据设置为 seoProps
-      setSeoProps(data);
-    };
-
-    // 调用数据获取函数
-    fetchData();
-  }, []); // 空数组表示仅在组件挂载时执行一次
-
+const Page = ({ seoProps }:any) => {
   return (
     <>
-      {/* 使用条件渲染，只有在 seoProps 不为 null 时才渲染 NextSeo */}
-      {seoProps !== null && (
+      {seoProps && (
         <NextSeo
           title={seoProps.title}
           description={seoProps.description}
@@ -70,5 +31,35 @@ const Page: React.FC = () => {
     </>
   );
 };
+
+export async function getServerSideProps() {
+  // 模拟异步操作，实际中根据需要获取数据
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+
+  // 返回页面所需数据
+  const seoProps = {
+    title: 'Holoworld AI- Chat',
+    description: 'Your description here',
+    canonical: 'Your canonical URL here',
+    openGraph: {
+      url: 'Your Open Graph URL here',
+      title: 'Your Open Graph Title',
+      description: 'Your Open Graph Description',
+      images: [
+        {
+          url: '/images/test.png',
+          alt: 'Your image alt text here',
+        },
+      ],
+      siteName: 'Your Site Name',
+    },
+  };
+
+  return {
+    props: {
+      seoProps,
+    },
+  };
+}
 
 export default Page;
