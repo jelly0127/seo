e7f4b042ff52c0d05872b4023911938911d5f9b6
jelly <806352173@qq.com>
2024-03-01 14:34:34 +0800

    Test

--- src/pages/a/index.tsx
+++ src/pages/a/index.tsx
@@ -1,5 +1,5 @@
 import { NextSeo } from 'next-seo';
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 interface SeoProps {
   title: string;
   description: string;
@@ -18,30 +18,28 @@ interface SeoProps {
 interface PageProps {
   seoProps: SeoProps;
 }
-const Page:FC<PageProps> = ({ seoProps }) => {
-  return (
-    <>
-      {seoProps && (
-        <NextSeo
-          title={seoProps.title}
-          description={seoProps.description}
-          canonical={seoProps.canonical}
-          openGraph={seoProps.openGraph}
-          // 其他可能的 NextSeo 配置项
-        />
-      )}
-      <p>SEO Added to Page</p>
-    </>
-  );
-};
+const Page: FC<PageProps> = () => {
+  const [seoProps, setSeoProps] = useState<SeoProps>({  title: '',
+  description: '',
+  canonical: '',
+  openGraph: {
+    url: '',
+    title: '',
+    description: '',
+    images:[ {
+      url: '',
+      alt: ''
+    }],
+    siteName: ''
+  }})
 
-export async function getServerSideProps() {
+   async function getData() {
   // 模拟异步操作，实际中根据需要获取数据
   await new Promise((resolve) => setTimeout(resolve, 2000));
 
   // 返回页面所需数据
-  const seoProps = {
-    title: 'Holoworld AI- Chat',
+  const seoPropsData = {
+    title: 'Holoworld AI- Chat B',
     description: 'Your description here',
     canonical: 'Your canonical URL here',
     openGraph: {
@@ -57,12 +55,56 @@ export async function getServerSideProps() {
       siteName: 'Your Site Name',
     },
   };
-
-  return {
-    props: {
-      seoProps,
-    },
-  };
+setSeoProps(seoPropsData)
+ 
 }
+  useEffect(() => { 
+    getData()
+  },[])
+  return (
+    <>
+      {seoProps && (
+        <NextSeo
+          title={seoProps.title}
+          description={seoProps.description}
+          canonical={seoProps.canonical}
+          openGraph={seoProps.openGraph}
+          // 其他可能的 NextSeo 配置项
+        />
+      )}
+      <p>SEO Added to Page B</p>
+    </>
+  );
+};
+
+// export async function getServerSideProps() {
+//   // 模拟异步操作，实际中根据需要获取数据
+//   await new Promise((resolve) => setTimeout(resolve, 2000));
+
+//   // 返回页面所需数据
+//   const seoProps = {
+//     title: 'Holoworld AI- Chat B',
+//     description: 'Your description here',
+//     canonical: 'Your canonical URL here',
+//     openGraph: {
+//       url: 'Your Open Graph URL here',
+//       title: 'Your Open Graph Title',
+//       description: 'Your Open Graph Description',
+//       images: [
+//         {
+//           url: '/images/test.png',
+//           alt: 'Your image alt text here',
+//         },
+//       ],
+//       siteName: 'Your Site Name',
+//     },
+//   };
+
+//   return {
+//     props: {
+//       seoProps,
+//     },
+//   };
+// }
 
 export default Page;
